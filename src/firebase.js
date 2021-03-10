f3893f3b85bf1fcb35570e8420227d1555622542
H0uss3m <hossem.benmustapha@gmail.com>
2021-03-10 13:26:06 +0100

    fix deploy

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,16 @@
-import firebase from "firebase";
-var provider = new firebase.auth.GoogleAuthProvider();
+import firebase from 'firebase'
+var provider = new firebase.auth.GoogleAuthProvider()
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBD5aVS1JDUvPKnlTG6NUcYwHIxVra9YGI",
-  authDomain: "houssem-ben-mustapha-45efa.firebaseapp.com",
-  projectId: "houssem-ben-mustapha-45efa",
-  storageBucket: "houssem-ben-mustapha-45efa.appspot.com",
-  messagingSenderId: "973163272756",
-  appId: "1:973163272756:web:7a508852e3d4e9c0cd0861",
-};
+  apiKey: 'AIzaSyDH8fqu4NdcYqCVMg9jWEyprE_uRTKEfjs',
+  authDomain: 'houssem-benmustapha.firebaseapp.com',
+  projectId: 'houssem-benmustapha',
+  storageBucket: 'houssem-benmustapha.appspot.com',
+  messagingSenderId: '409651127122',
+  appId: '1:409651127122:web:998ae46964f1fdbb1d908b',
+}
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-const db = firebaseApp.firestore();
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
 
-export default db;
+export default db
